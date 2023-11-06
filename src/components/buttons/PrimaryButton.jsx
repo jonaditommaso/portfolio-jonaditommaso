0d48386e8021c350ff08f9f52d183651b79d06c6
jonaditommaso <jonaditommaso@gmail.com>
2023-11-06 19:50:54 -0300

    [main] chore: apply styles to buttons with mui approach

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,12 +1,19 @@
 import Button from '@mui/material/Button';
 import styles from './styles.module.css';
+import { styled } from '@mui/material/styles';
+
+const MuiButton = styled(Button)(({ theme }) => ({
+    color: 'white',
+    backgroundColor: '#160e08',
+    fontFamily: 'Montserrat, sans-serif',
+}));
 
 const PrimaryButton = ({ onClick, children }) => {
 
     return (
-        <Button size="small" variant="outlined" onClick={onClick} className={styles['primary-portfolio-button']}>
+        <MuiButton size="small" variant="outlined" onClick={onClick} className={styles['primary-button']}>
             {children}
-        </Button>
+        </MuiButton>
     );
 }
 
--- src/components/buttons/SecondaryButton.jsx
+++ src/components/buttons/SecondaryButton.jsx
@@ -1,12 +1,23 @@
 import Button from '@mui/material/Button';
 import styles from './styles.module.css';
+import { styled } from '@mui/material/styles';
+
+const MuiButton = styled(Button)(({ theme }) => ({
+    backgroundColor: 'white',
+    color: 'black',
+    margin: '10px',
+    fontFamily: 'Montserrat, sans-serif',
+    '&:hover': {
+        transition: '0.3s'
+      },
+}));
 
 const SecondaryButton = ({ children, onClick }) => {
 
     return (
-        <Button size="small" variant="contained" className={styles['secondary-portfolio-button']} onClick={onClick ? onClick : undefined}>
+        <MuiButton size="small" variant="contained" className={styles['secondary-button']} onClick={onClick ? onClick : undefined}>
             {children}
-        </Button>
+        </MuiButton>
     );
 }
 
